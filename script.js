14a3f6cb7ff7fecf52a585f37d126548a42cbf1d
Nikhil <nikhil.bhatt.000@gmail.com>
2021-09-21 18:57:20 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  autoPlay:false,
+  draggable: true,
+  prevNextButtons: true,
+  wrapAround: true
+});
